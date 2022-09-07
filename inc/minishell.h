8310524ff3100d8441268b146dc76c01dc082e62
jseijo-p <jseijo-p@student.42malaga.com>
2022-09-07 11:25:14 +0200

    Initial structs for commands as stated on guide: https://www.cs.purdue.edu/homes/grr/SystemsProgrammingBook/Book/Chapter5-WritingYourOwnShell.pdf

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jseijo-p <jseijo-p@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 08:57:42 by jseijo-p          #+#    #+#             */
-/*   Updated: 2022/09/07 11:02:28 by jseijo-p         ###   ########.fr       */
+/*   Updated: 2022/09/07 11:23:47 by jseijo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,26 @@
 
 #include <stdio.h>
 
-struct s_command {
+typedef struct s_simple_command {
 		// Available space for arguments currently preallocated
 		int _numberOfAvailableArguments;
 		// Number of arguments
 		int _numberOfArguments;
 		// Array of arguments
 		char ** _arguments;
-		// SimpleCommand();
-		// void insertArgument(char *argument);
+} t_simple_command;
+
+typedef struct s_command {
+	int _numberOfAvailableSimpleCommands;
+	int _numberOfSimpleCommands;
+	t_simple_command **_simpleCommands;
+	char *_outFile;
+	char *_inputFile;
+	char *_errFile;
+	int _background;
+
+	struct s_command *_currentCommand;
+	t_simple_command *_currentSimpleCommand;
 } t_command;
 
 int parser(int argc, char **argv, char **envp);
--- src/command.c
+++ src/command.c
@@ -6,10 +6,56 @@
 /*   By: jseijo-p <jseijo-p@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 10:53:59 by jseijo-p          #+#    #+#             */
-/*   Updated: 2022/09/07 11:00:50 by jseijo-p         ###   ########.fr       */
+/*   Updated: 2022/09/07 11:21:28 by jseijo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/minishell.h"
+
 /*
-** File for working with commands
+** SimpleCommand/s
 */
+
+void simple_command()
+{
+
+}
+
+void insertArgument(char *argument)
+{
+	printf("insert_argument(): %s\n", argument);
+}
+
+/*
+** Command processor
+*/
+
+void prompt()
+{
+
+}
+
+void print()
+{
+
+}
+
+void execute()
+{
+
+}
+
+void clear()
+{
+
+}
+
+void command()
+{
+
+}
+
+void insertSimpleCommand( t_simple_command *simpleCommand )
+{
+
+}
